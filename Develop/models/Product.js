1f26d8e625bc0afab52d9970360f6ac650fb5ef6
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-08-09 14:01:31 -0400

    updated category model values

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,5 +1,5 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -9,7 +9,11 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id:{
+      type:DataTypes.INTEGER,
+      
+
+    }
   },
   {
     sequelize,
