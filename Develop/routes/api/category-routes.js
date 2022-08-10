908b6eb66dcd31d099ca1eaf225b4d3ea6fbf489
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-08-10 15:09:12 -0400

    fixed put issues

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -36,7 +36,7 @@ router.post('/', (req, res) => {
     const categories= await Category.create(req.body);
     res.status(200).json(categories)
   } catch (err) {
-    res.status(500).json(err)
+    res.status(400).json(err)
   }
 });
 
@@ -46,9 +46,9 @@ router.put('/:id', (req, res) => {
     const categories=await Category.update(req.params.id,{
       include:[{model:Product}],
     })
-    res.status(200).json(categories)
+    res.status(200).json({message: "Category updated successfully"})
   }catch(err){
-    res.status(200).json(categories)
+    res.status(400).json(err)
   }
 });
 
@@ -66,7 +66,7 @@ router.delete('/:id', (req, res) => {
     }
     res.status(200).json(categories);
   } catch (err){
-    res.status(500).json(err);  
+    res.status(400).json(err);  
 }
 });
 
