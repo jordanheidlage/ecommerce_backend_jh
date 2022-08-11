a066b6487313b58bcbf1425099f96fe21598c399
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-08-10 20:50:56 -0400

    adding async function to tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,7 +29,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try {
     const tags = await Tag.create(req.body);
@@ -39,7 +39,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
     await Tag.update(req.body, {
@@ -53,7 +53,7 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
     await Tag.destroy({
