5626b615921f47fd01d25057addc0c351ce1f9cd
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-08-29 13:47:41 -0400

    updated tagroutes to add req.params.id to get by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,9 +4,9 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', async (req, res) => {
-  try{
-    const tags=await Tag.findAll({
-      include:[{model:Product, through:ProductTag, as: 'products'}],
+  try {
+    const tags = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag, as: 'products' }],
     })
     res.status(200).json(tags)
   } catch (err) {
@@ -19,9 +19,9 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  try{
-    const tags=await Tag.findByPk({
-      include:[{model:Product, through:ProductTag, as: 'products'}],
+  try {
+    const tags = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'products' }],
     })
     res.status(200).json(tags)
   } catch (err) {
@@ -32,10 +32,10 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tags = await Tag.create(req.body);
-    res.status(200).json(tags);
+    const tags = await Tag.create(req.body)
+    res.status(200).json(tags)
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json(err)
   }
 });
 
@@ -47,8 +47,8 @@ router.put('/:id', async (req, res) => {
         id: req.params.id,
       }
     })
-    res.status(200).json({message: "Tag updated successfully!"})
-  } catch (err){
+    res.status(200).json({ message: "Tag updated successfully!" })
+  } catch (err) {
     res.status(400).json(err)
   }
 });
@@ -61,8 +61,8 @@ router.delete('/:id', async (req, res) => {
         id: req.params.id,
       }
     })
-    res.status(200).json({message: "Tag deleted successfully!"})
-  } catch (err){
+    res.status(200).json({ message: "Tag deleted successfully!" })
+  } catch (err) {
     res.status(400).json(err)
   }
 });
